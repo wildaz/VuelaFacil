5da0fd9804fdc70d7277f907e2d078805be7626c
Katherin Reyes <kajorey@gmail.com>
2021-09-06 15:52:57 -0500

    Prueba KR

---
+++ Vuelos.py
@@ -0,0 +1,11 @@
+class Reg_Vuelos:
+    Aviones=0
+    sillas=0
+    trayectos=""
+
+    def _init_(self,Aviones,sillas,trayectos):
+        self.Aviones=aviones
+        self.sillas=sillas
+        self.trayectos=trayectos
+        
+        
